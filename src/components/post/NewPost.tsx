02f26144a3342a9b3552fefcc8490ed0f116872e
Attila Csanyi <attila.csnyi@yahoo.co.uk>
2024-12-01 17:53:58 +0100

    feat: enhance NewPost component with controlled inputs and update PostList to manage state (parent - child)

--- src/components/post/NewPost.tsx
+++ src/components/post/NewPost.tsx
@@ -1,15 +1,18 @@
 import classes from "./NewPost.module.css";
 
-const NewPost = () => {
+const NewPost = (props: {
+  onChangeBody: React.ChangeEventHandler<HTMLTextAreaElement>;
+  onChangeAuthor: React.ChangeEventHandler<HTMLInputElement>;
+}) => {
   return (
     <form className={classes.form}>
       <p>
         <label htmlFor="body">Text</label>
-        <textarea id="body" required rows={3} />
+        <textarea id="body" required rows={3} onChange={props.onChangeBody} />
       </p>
       <p>
         <label htmlFor="name">Your name</label>
-        <input type="text" id="name" required />
+        <input type="text" id="name" required onChange={props.onChangeAuthor} />
       </p>
     </form>
   );
--- src/components/post/PostList.tsx
+++ src/components/post/PostList.tsx
@@ -1,13 +1,20 @@
+import { useState } from "react";
 import NewPost from "./NewPost";
 import Post from "./Post";
 import styles from "./PostList.module.css";
 
 const PostList = () => {
+  const [enteredBody, setEnteredBody] = useState("Default Body");
+  const [enteredAuthor, setEnteredAuthor] = useState("Default Author");
+
   return (
     <>
-      <NewPost />
+      <NewPost
+        onChangeBody={(event) => setEnteredBody(event.target.value)}
+        onChangeAuthor={(event) => setEnteredAuthor(event.target.value)}
+      />
       <ul className={styles.posts}>
-        <Post author="John Doe" content="This is a post" />
+        <Post author={enteredAuthor} content={enteredBody} />
         <Post author="Jane Doe" content="This is another post" />
       </ul>
     </>
